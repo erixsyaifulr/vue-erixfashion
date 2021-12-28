22e6ec9352e467fa917fb67e86ecff68d07d1c88
erixsyaifulr <syaifulerix@gmail.com>
2021-12-28 20:29:14 +0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="home">
+    <HeaderErixCloth />
+    <SlideCarousel />
+    <ProductSlider />
+    <InstaErixFashion />
+    <PartnerLogo />
+    <Footer />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HeaderErixCloth from "@/components/HeaderErixCloth.vue";
+import SlideCarousel from "@/components/SlideCarousel.vue";
+import ProductSlider from "@/components/ProductSlider.vue";
+import InstaErixFashion from "@/components/InstaErixFashion.vue";
+import PartnerLogo from "@/components/PartnerLogo.vue";
+import Footer from "@/components/Footer.vue";
+
+export default {
+  name: "Home",
+  components: {
+    HeaderErixCloth,
+    SlideCarousel,
+    ProductSlider,
+    InstaErixFashion,
+    PartnerLogo,
+    Footer,
+  },
+};
+</script>
